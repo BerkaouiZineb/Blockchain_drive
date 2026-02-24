b1e0b008bff66f50645033c843ac2e71ec164c83
berkaouizineb <berkaouizineb99@gmail.com>
2026-02-24 12:37:44 +0000

    Initial commit - projet blockchain

---
+++ client/src/utils/constants.js
@@ -0,0 +1,18 @@
+
+
+// ! ||--------------------------------------------------------------------------------||
+// ! ||                                 Smart Contract                                 ||
+// ! ||--------------------------------------------------------------------------------||
+
+import abi from './upload.json'
+export const  contractAddress= '0x5FbDB2315678afecb367f032d93F642f64180aa3';
+export const contractAbi = abi;
+
+
+// ! ||--------------------------------------------------------------------------------||
+// ! ||                                   Pinata IPFS                                  ||
+// ! ||--------------------------------------------------------------------------------||
+
+export const API_Key =  'f77756133947bdf523e9'
+export const API_Secret=  '6bd9409ab966c467beb0f45a2536f48dbc3070be587e06d2fbba0b289a077db4'
+export const JWT= 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiIzMmM4ZWI4OC1iZDY0LTQ4NDctYmMzYS02YzMyZjg0ODNmMmUiLCJlbWFpbCI6ImJlcmthb3VpemluZWI5OUBnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwicGluX3BvbGljeSI6eyJyZWdpb25zIjpbeyJkZXNpcmVkUmVwbGljYXRpb25Db3VudCI6MSwiaWQiOiJGUkExIn0seyJkZXNpcmVkUmVwbGljYXRpb25Db3VudCI6MSwiaWQiOiJOWUMxIn1dLCJ2ZXJzaW9uIjoxfSwibWZhX2VuYWJsZWQiOmZhbHNlLCJzdGF0dXMiOiJBQ1RJVkUifSwiYXV0aGVudGljYXRpb25UeXBlIjoic2NvcGVkS2V5Iiwic2NvcGVkS2V5S2V5IjoiZjc3NzU2MTMzOTQ3YmRmNTIzZTkiLCJzY29wZWRLZXlTZWNyZXQiOiI2YmQ5NDA5YWI5NjZjNDY3YmViMGY0NWEyNTM2ZjQ4ZGJjMzA3MGJlNTg3ZTA2ZDJmYmJhMGIyODlhMDc3ZGI0IiwiZXhwIjoxODAxOTM4MDc3fQ.uaor2-3VY3Nmsuixc_HuGU6rXuKKnCXhK_Gr9WkWsRU.G7gsJwNXqXfzExHBWjnDBI7aasvFCluB8puEMV7OZhY'
